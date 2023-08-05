11916f0ba3a7958f2c5977b1c68c3a8bbdbdc57b
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-04 21:42:44 -0300

    Update main.js

--- jsproject/main.js
+++ jsproject/main.js
@@ -17,3 +17,4 @@ const deposit = () => {
 const getNumberOfLines = () => {
     while (true) {
         const lines = prompt('Enter the number of lines to bet (1-3): ');
+        const numberOfLines = parseFloat(lines);
