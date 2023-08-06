bf31601281b31893ab2da6e9e7399d4b5f737d00
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-06 18:54:43 -0300

    Update scripts.js

--- jsproject/js/scripts.js
+++ jsproject/js/scripts.js
@@ -1,3 +1,4 @@
 // Selection
 const todoForm = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
