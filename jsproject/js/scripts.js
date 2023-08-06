119879da7b0e0cea4429f1a54d389daff8bdd514
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-06 18:57:46 -0300

    Update scripts.js

--- jsproject/js/scripts.js
+++ jsproject/js/scripts.js
@@ -20,3 +20,4 @@ const saveTodo = (Text) => {
 
     const doneBtn = document.createElement("button")
     doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
