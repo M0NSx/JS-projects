a18f5f9617a416b0fa7c6dcee6791d3c0e80a1df
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-06 18:55:18 -0300

    Update scripts.js

--- jsproject/js/scripts.js
+++ jsproject/js/scripts.js
@@ -5,3 +5,5 @@ const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditButton = document.querySelector("#cancel-edit-btn");
+
+let oldInputValue;
