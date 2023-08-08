4e237f9a99e43ed9c87cd0a9b25fc6e592de78dc
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-08-07 21:21:17 -0300

    Update scripts.js

---
+++ jsproject2/js/scripts.js
@@ -0,0 +1,47 @@
+/* Selection */
+const todoForm = document.querySelector("#todo-form")
+const todoInput = document.querySelector("#todo-input")
+const editForm = document.querySelector("#edit-form")
+const editInput = document.querySelector("#edit-input")
+const todoList = document.querySelector("#todo-list")
+const cancelEditBtn = document.querySelector("#cancel-edit-btn")
+
+/* Functions */
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    doneBtn.appendChild(todo)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    editBtn.appendChild(todo)
+
+    const removeBtn = document.createElement("button")
+    removeBtn.classList.add("remove-todo")
+    removeBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    removeBtn.appendChild(todo)
+
+}
+
+/* Events */
+todoForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    const inputValue = todoInput.value
+
+    if (inputValue) {
+        saveTodo(inputValue)
+    }
+
+})
