6d9ea6d2177095b2ed7cb8eb03ab56d1b6d7b50a
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-01 18:09:41 -0300

    Update Login.jsx

--- Login Page2/client/src/Login.jsx
+++ Login Page2/client/src/Login.jsx
@@ -2,3 +2,4 @@ import React, { useState } from 'react';
 import './Signup.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import ImageL from './img/Site-logo.png';
+import ImageBG from './img/BF-BG2.png';
