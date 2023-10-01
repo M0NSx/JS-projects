2b07ff0888fdef45ff13fe794df8320ee5bb105a
M0NSx <137951293+M0NSx@users.noreply.github.com>
2023-10-01 18:09:45 -0300

    Update Login.jsx

--- Login Page2/client/src/Login.jsx
+++ Login Page2/client/src/Login.jsx
@@ -3,3 +3,4 @@ import './Signup.css';
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
 import ImageL from './img/Site-logo.png';
 import ImageBG from './img/BF-BG2.png';
+import { Link } from 'react-router-dom';
